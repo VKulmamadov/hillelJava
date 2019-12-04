7bb7bd04c6948b45dca87543049c2c139182f7fb
VKulmamadov <vlad.kulmamadov@gmail.com>
2019-12-04 21:26:44 +0200

    fix div

--- calc/Main.java
+++ calc/Main.java
@@ -9,14 +9,17 @@ public class Main {
         System.out.println("Please input second number:");
         int num2 = in.nextInt();
         System.out.printf("Your input %d as a second number. \n", num2);
-        int sum = num1+num2;
+        long sum = num1+num2;
         System.out.printf("Your \nSum is %d \n", sum);
-        int dif = num1-num2;
+        long dif = num1-num2;
         System.out.printf("Diference is %d \n", dif);
-        int mul = num1*num2;
+        long mul = num1*num2;
         System.out.printf("Multiplication is %d \n", mul);
-        int div = num1*num2;
-        System.out.printf("Division is %d \n", div);
-
+        if (num2 != 0) {
+            long div = num1 / num2;
+            System.out.printf("Division is %d \n", div);
+        } else {
+            System.out.print("Division impossible");
+        }
     }
 }
